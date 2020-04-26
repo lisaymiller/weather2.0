95a4f3c1f74a41f16e5c60d11adc8ebdb051c793
Lisa Miller <lisayevonne@outlook.com>
2020-04-26 12:37:13 -0400

    Updates

---
+++ precache-manifest.3b593aa2db9c48ad2a894075ea9bd8d7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d432558883fa5c82b920b885958ccb",
+    "url": "/weather2.0/index.html"
+  },
+  {
+    "revision": "913ce3bb89ddf9a0b0af",
+    "url": "/weather2.0/static/css/main.b8fce3e3.chunk.css"
+  },
+  {
+    "revision": "fe04101561412705f400",
+    "url": "/weather2.0/static/js/2.5ffd555d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather2.0/static/js/2.5ffd555d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913ce3bb89ddf9a0b0af",
+    "url": "/weather2.0/static/js/main.a1f7c65d.chunk.js"
+  },
+  {
+    "revision": "410a3a56dc56eb82b253",
+    "url": "/weather2.0/static/js/runtime-main.f1ba388a.js"
+  },
+  {
+    "revision": "f52851dbb10046960c2694413bb4f409",
+    "url": "/weather2.0/static/media/background.f52851db.jpg"
+  },
+  {
+    "revision": "52f92b0930548fc3604fbc9f6905e385",
+    "url": "/weather2.0/static/media/cold.52f92b09.jpg"
+  },
+  {
+    "revision": "9eae65fbd848f7e442dd7de76afebd50",
+    "url": "/weather2.0/static/media/cool.9eae65fb.jpg"
+  },
+  {
+    "revision": "25e35e450216fe7d8e312baeef89d1d2",
+    "url": "/weather2.0/static/media/frozen.25e35e45.jpg"
+  },
+  {
+    "revision": "789ab914da041d306bac0352dde1d03f",
+    "url": "/weather2.0/static/media/hot.789ab914.jpg"
+  },
+  {
+    "revision": "4c1af54749bb4ee81f5716569cce1e37",
+    "url": "/weather2.0/static/media/snowing.4c1af547.jpg"
+  },
+  {
+    "revision": "e319b0ab73b22b363766cd283f650103",
+    "url": "/weather2.0/static/media/tempid.e319b0ab.jpg"
+  },
+  {
+    "revision": "2c835837781c78dd1253ae0eb8478f68",
+    "url": "/weather2.0/static/media/veryhot.2c835837.jpg"
+  },
+  {
+    "revision": "5bb58250a608dafe52c0555e8653619a",
+    "url": "/weather2.0/static/media/warm.5bb58250.jpg"
+  }
+]);
